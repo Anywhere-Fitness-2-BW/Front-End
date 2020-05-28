67024add5980073bb6b2b8a87ca79da5df19a6d1
meloegel <meloegel@gmail.com>
2020-05-28 12:54:56 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -7,89 +7,88 @@ import { NavContext } from "./contexts/NavContext";
 import PrivateRoute from "./components/Protected/PrivateRoute";
 
 import Home from "./components/Home";
-import Login from "./components/Login-mark";
+import Login from "./components/Login";
 import Navigation from "./components/Navigation";
 import Client from "./components/Protected/Client/Client";
 import Instructor from "./components//Protected/Instructor/Instructor";
-// import CreateClass from "./components/Protected/Instructor/CreateClass";
+import CreateClass from "./components/Protected/Instructor/CreateClass";
 import EditClass from "./components/Protected/Instructor/EditClass";
-
 const sampleClassList = [
-{
-  id: 0,
-  name: "class 0",
-  coursDetails:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quis orci varius sem euismod gravida. Nulla ac diam eu urna suscipit ultricies. Nullam mi mauris, blandit non enim nec, pulvinar vulputate tellus. Cras bibendum justo in tincidunt euismod. Duis a velit gravida, vehicula ex nec, ultrices nulla.",
-  instructor: "instructor 0",
-  type: "yoga",
-  date: "01-20-2020",
-  time: "10:00",
-  duration: "30",
-  intensity: "beginner",
-  location: "1 street, Seattle, WA",
-  registered: 0,
-  max: 20,
-},
-{
-  id: 1,
-  name: "class 1",
-  coursDetails:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quis orci varius sem euismod gravida. Nulla ac diam eu urna suscipit ultricies. Nullam mi mauris, blandit non enim nec, pulvinar vulputate tellus. Cras bibendum justo in tincidunt euismod. Duis a velit gravida, vehicula ex nec, ultrices nulla.",
-  instructor: "instructor 1",
-  type: "boxing",
-  date: "01-23-2020",
-  time: "12:00",
-  duration: "30",
-  intensity: "intermediate",
-  location: "2 street, Seattle, WA",
-  registered: 5,
-  max: 15,
-},
-{
-  id: 2,
-  name: "class 2",
-  coursDetails:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quis orci varius sem euismod gravida. Nulla ac diam eu urna suscipit ultricies. Nullam mi mauris, blandit non enim nec, pulvinar vulputate tellus. Cras bibendum justo in tincidunt euismod. Duis a velit gravida, vehicula ex nec, ultrices nulla.",
-  instructor: "instructor 2",
-  type: "cardio",
-  date: "05-25-2020",
-  time: "10:00",
-  duration: "30",
-  intensity: "advanced",
-  location: "3 street, Portland, OR",
-  registered: 5,
-  max: 15,
-},
-{
-  id: 3,
-  name: "class 3",
-  coursDetails:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quis orci varius sem euismod gravida. Nulla ac diam eu urna suscipit ultricies. Nullam mi mauris, blandit non enim nec, pulvinar vulputate tellus. Cras bibendum justo in tincidunt euismod. Duis a velit gravida, vehicula ex nec, ultrices nulla.",
-  instructor: "instructor 3",
-  type: "boxing",
-  date: "05-23-2020",
-  time: "18:00",
-  duration: "30",
-  intensity: "advanced",
-  location: "53 street, Portland, OR",
-  registered: 5,
-  max: 15,
-},
-{
-  id: 4,
-  name: "class 4",
-  coursDetails:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quis orci varius sem euismod gravida. Nulla ac diam eu urna suscipit ultricies. Nullam mi mauris, blandit non enim nec, pulvinar vulputate tellus. Cras bibendum justo in tincidunt euismod. Duis a velit gravida, vehicula ex nec, ultrices nulla.",
-  instructor: "instructor 4",
-  type: "dance",
-  date: "05-23-2020",
-  time: "18:00",
-  duration: "15",
-  intensity: "beginner",
-  location: "23 street, Los Angeles, CA",
-  registered: 2,
-  max: 5,
-},
+  {
+    id: 0,
+    name: "class 0",
+    coursDetails:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quis orci varius sem euismod gravida. Nulla ac diam eu urna suscipit ultricies. Nullam mi mauris, blandit non enim nec, pulvinar vulputate tellus. Cras bibendum justo in tincidunt euismod. Duis a velit gravida, vehicula ex nec, ultrices nulla.",
+    instructor: "instructor 0",
+    type: "yoga",
+    date: "01-20-2020",
+    time: "10:00",
+    duration: "30",
+    intensity: "beginner",
+    location: "1 street, Seattle, WA",
+    registered: 0,
+    max: 20,
+  },
+  {
+    id: 1,
+    name: "class 1",
+    coursDetails:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quis orci varius sem euismod gravida. Nulla ac diam eu urna suscipit ultricies. Nullam mi mauris, blandit non enim nec, pulvinar vulputate tellus. Cras bibendum justo in tincidunt euismod. Duis a velit gravida, vehicula ex nec, ultrices nulla.",
+    instructor: "instructor 1",
+    type: "boxing",
+    date: "01-23-2020",
+    time: "12:00",
+    duration: "30",
+    intensity: "intermediate",
+    location: "2 street, Seattle, WA",
+    registered: 5,
+    max: 15,
+  },
+  {
+    id: 2,
+    name: "class 2",
+    coursDetails:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quis orci varius sem euismod gravida. Nulla ac diam eu urna suscipit ultricies. Nullam mi mauris, blandit non enim nec, pulvinar vulputate tellus. Cras bibendum justo in tincidunt euismod. Duis a velit gravida, vehicula ex nec, ultrices nulla.",
+    instructor: "instructor 2",
+    type: "cardio",
+    date: "05-25-2020",
+    time: "10:00",
+    duration: "30",
+    intensity: "advanced",
+    location: "3 street, Portland, OR",
+    registered: 5,
+    max: 15,
+  },
+  {
+    id: 3,
+    name: "class 3",
+    coursDetails:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quis orci varius sem euismod gravida. Nulla ac diam eu urna suscipit ultricies. Nullam mi mauris, blandit non enim nec, pulvinar vulputate tellus. Cras bibendum justo in tincidunt euismod. Duis a velit gravida, vehicula ex nec, ultrices nulla.",
+    instructor: "instructor 3",
+    type: "boxing",
+    date: "05-23-2020",
+    time: "18:00",
+    duration: "30",
+    intensity: "advanced",
+    location: "53 street, Portland, OR",
+    registered: 5,
+    max: 15,
+  },
+  {
+    id: 4,
+    name: "class 4",
+    coursDetails:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quis orci varius sem euismod gravida. Nulla ac diam eu urna suscipit ultricies. Nullam mi mauris, blandit non enim nec, pulvinar vulputate tellus. Cras bibendum justo in tincidunt euismod. Duis a velit gravida, vehicula ex nec, ultrices nulla.",
+    instructor: "instructor 4",
+    type: "dance",
+    date: "05-23-2020",
+    time: "18:00",
+    duration: "15",
+    intensity: "beginner",
+    location: "23 street, Los Angeles, CA",
+    registered: 2,
+    max: 5,
+  },
 ];
 
 function App() {
